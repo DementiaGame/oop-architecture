597c1a43691fe48d93a13ec205103b63ebf0f694
euics <pjh575157@gmail.com>
2024-07-10 00:53:23 +0900

    feat: MemberService에 로그인 기능 추가 (#5)
    - MemberLoginReqDto를 받아 로그인 처리하는 loginMemberEntity 메서드 구현
      - 닉네임을 사용한 회원 조회 및 유효성 검사
      - JWT 토큰 생성 (AccessToken 및 RefreshToken)
      - 생성된 토큰을 Redis에 저장 (MemberDomain을 통해 처리)
      - MemberLoginResDto를 반환하여 로그인 결과 전달

--- service/src/main/java/way/application/service/member/service/MemberService.java
+++ service/src/main/java/way/application/service/member/service/MemberService.java
@@ -4,17 +4,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import way.application.domain.member.MemberDomain;
 import way.application.infrastructure.member.entity.MemberEntity;
 import way.application.infrastructure.member.repository.MemberRepository;
 import way.application.service.member.dto.MemberDto;
+import way.application.service.member.dto.req.MemberLoginReqDto;
+import way.application.service.member.dto.res.MemberLoginResDto;
 import way.application.service.member.mapper.MemberMapper;
+import way.application.utils.jwt.JwtTokenUtils;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 	private final MemberRepository memberRepository;
+	private final JwtTokenUtils jwtTokenUtils;
 
 	private final MemberMapper memberMapper;
+
+	private final MemberDomain memberDomain;
 
 	@Transactional
 	public Long signUpMemberEntity(MemberDto memberDto) {
@@ -25,5 +32,20 @@ public class MemberService {
 		MemberEntity savedMemberEntity = memberRepository.saveMemberEntity(memberMapper.toMemberEntity(memberDto));
 
 		return savedMemberEntity.getMemberSeq();
+	}
+
+	@Transactional
+	public MemberLoginResDto loginMemberEntity(MemberLoginReqDto memberLoginReqDto) {
+		// Nick Name 유효성 검사
+		MemberEntity memberEntity = memberRepository.findByMemberNickName(memberLoginReqDto.nickName());
+
+		// Token 발급 (Nick Name이 Unique 하므로 Nick Name으로 발급)
+		String accessToken = jwtTokenUtils.generateAccessToken(memberEntity.getNickName());
+		String refreshToken = jwtTokenUtils.generateRefreshToken(memberEntity.getNickName());
+
+		// Service 계층에서 Redis 저장이 아닌 Domain 에서 책임 부여
+		memberDomain.saveTokenInRedis(memberEntity.getNickName(), accessToken, refreshToken);
+
+		return new MemberLoginResDto(memberEntity.getMemberSeq(), accessToken, refreshToken);
 	}
 }
