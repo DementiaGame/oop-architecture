cbb91cafdb1f903c00b6c1b2cb99eab97bf2d31d
euics <pjh575157@gmail.com>
2024-07-09 23:53:04 +0900

    feat: MemberRepository 구현 및 회원 닉네임 검증 로직 추가 (#3)
    - MemberJpaRepository 인터페이스 생성: JpaRepository를 상속받아 MemberEntity의 기본 CRUD 작업을 처리할 수 있도록 함. 또한, 닉네임으로 회원을 조회하는 메서드(findByNickName)를 추가함.
    - MemberRepository 인터페이스 생성: 회원 닉네임 검증(validateMemberNickName)과 회원 엔티티 저장(saveMemberEntity) 메서드를 정의함.
    - MemberRepositoryImpl 클래스 생성: MemberRepository 인터페이스를 구현하여, 닉네임 중복 여부를 검증하는 로직을 추가함. 중복된 닉네임이 존재할 경우 ConflictException을 발생시키도록 구현. 또한, 회원 엔티티를 저장하는 메서드를 구현하여, JpaRepository를 통해 엔티티를 저장하도록 함.
    - 필요한 예외 클래스 및 에러 결과 클래스 추가: BadRequestException, ConflictException, ErrorResult를 추가하여 예외 상황을 처리하도록 함.

--- infrastructure/src/main/java/way/application/infrastructure/member/repository/MemberJpaRepository.java
+++ infrastructure/src/main/java/way/application/infrastructure/member/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package way.application.infrastructure.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import way.application.infrastructure.member.entity.MemberEntity;
 
 @Repository
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
-
+	Optional<MemberEntity> findByNickName(String nickName);
 }
